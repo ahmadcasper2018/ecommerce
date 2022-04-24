228fef52f9e536e296286e7bc932c88e5d4cc052
Ahmad_Salman2021 <ahmad.salman@kuwaitnet.dev>
2022-04-24 13:54:39 +0300

    feat: add phone categorty

--- core/models.py
+++ core/models.py
@@ -9,7 +9,8 @@ from django_countries.fields import CountryField
 CATEGORY_CHOICES = (
     ('S', 'Shirt'),
     ('SW', 'Sport wear'),
-    ('OW', 'Outwear')
+    ('OW', 'Outwear'),
+    ('m', 'Phone')
 )
 
 LABEL_CHOICES = (
